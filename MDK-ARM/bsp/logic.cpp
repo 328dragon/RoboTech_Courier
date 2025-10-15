a825d871e797148ff7ae1e7015a73d445262e3ee
Nagisa <2964793117@qq.com>
2025-10-15 15:47:21 +0800

    增加了初版逻辑编写

---
+++ MDK-ARM/bsp/logic.cpp
@@ -0,0 +1,127 @@
+/*
+ * @Author: Nagisa 2964793117@qq.com
+ * @Date: 2025-10-13 19:15:07
+ * @LastEditors: Nagisa 2964793117@qq.com
+ * @LastEditTime: 2025-10-14 21:20:08
+ * @FilePath: \MDK-ARM\bsp\logic.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/*
+ * @Author: Nagisa 2964793117@qq.com
+ * @Date: 2025-10-13 19:15:07
+ * @LastEditors: Nagisa 2964793117@qq.com
+ * @LastEditTime: 2025-10-13 21:40:49
+ * @FilePath: \MDK-ARM\bsp\logic.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+// 技能赛智慧快递的逻辑文件+接口的实现
+// 请注意，本文件的基础在车只有前向灰度和超声波传感器（一样一个）的基础上进行编写的
+#include "logic.h"
+bool Maze::detect_obstacle()
+{
+    // 测量车的前方是否有障碍物
+    if (_current_distance <= _block_distance ) 
+    {
+        // 如果当前距离小于阈值，说明前方有障碍物
+        if(_current_dir == UP) 
+        {
+            _map[_x-1][_y] = 1; // 在地图上标记障碍物
+        } 
+        else if (_current_dir == DOWN) 
+        {
+            _map[_x+1][_y] = 1; // 在地图上标记障碍物
+        } 
+        else if (_current_dir == LEFT) 
+        {
+            _map[_x][_y-1] = 1; // 在地图上标记障碍物
+        } 
+        else if (_current_dir == RIGHT) 
+        {
+            _map[_x][_y+1] = 1; // 在地图上标记障碍物
+        }
+        return true; // 返回探测到障碍物
+    }
+    return false; // 返回未探测到障碍物
+}
+/*
+    极其重要的避障函数，在每次转向后都需要执行一次
+*/
+void Maze::update_next_dir() 
+{
+    bool found_obstacle = detect_obstacle(); // 探测障碍物,首先看当前方向前一格是否有障碍物
+    //然后看当前是否在边界,如果在边界则禁止出界方向
+    if(_x==0)
+    {
+         _forbid_dir = UP;
+    }
+    if(_x==4)
+    {
+         _forbid_dir = DOWN;
+    }
+    if(_y==0 && _whichground==1)
+    {
+         _forbid_dir = LEFT;
+    }
+    if(_y==7 && _whichground==0)
+    {
+         _forbid_dir = RIGHT;
+    }
+    //如果没有障碍，将current_dir设为_prior_dir数组中从头到尾遍历第一个不和_forbid_dir相同的方向
+    //并在此逻辑中恢复堵车状态，但不改变此时的current_dir
+    if(!found_obstacle)
+    {
+        if(_be_block) // 如果之前是堵车状态，说明现在已经通了
+        {
+            _be_block_time = 0; // 堵车次数清零
+        }
+        if(!_be_block)
+        {
+            for(int i=0;i<4;i++)
+            {
+                if(_prior_dir[i]!=_forbid_dir)
+                {
+                    _current_dir = _prior_dir[i];
+                    break;
+                }
+            }
+        }
+        _be_block = false; // 堵车状态
+    }
+    if(found_obstacle)
+    {
+        _be_block = true; // 堵车状态
+        _be_block_time++;
+        // 从_prior_dir第_be_block_time个开始遍历，找到第一个不和_forbid_dir相同的方向
+        for(int i=_be_block_time;i<4;i++)
+        {
+            if(_prior_dir[i]!=_forbid_dir)
+            {
+                _current_dir = _prior_dir[i];
+                break;
+            }
+        }
+    }
+}
+/*
+    逻辑顺序为，每次运动一步，在运动前首先探测障碍物，刷新要走的方向，并转向，然后走一步，然后更新当前位置，循环往复
+*/
+void Maze::update_self_position() 
+{
+    // 根据当前方向更新位置
+    if (_current_dir == UP) 
+    {
+        _x -= 1; // 向上移动一格
+    } 
+    else if (_current_dir == DOWN) 
+    {
+        _x += 1; // 向下移动一格
+    } 
+    else if (_current_dir == LEFT) 
+    {
+        _y -= 1; // 向左移动一格
+    } 
+    else if (_current_dir == RIGHT) 
+    {
+        _y += 1; // 向右移动一格
+    }
+}
---
+++ MDK-ARM/bsp/logic.h
@@ -0,0 +1,54 @@
+#ifndef __LOGIC_H
+#define __LOGIC_H
+#include <array>
+enum Dir 
+    { 
+        UP, 
+        DOWN, 
+        LEFT, 
+        RIGHT 
+    };
+/*
+    迷宫类，包含迷宫地图和相关操作
+*/
+class Maze {
+    public:
+        int _whichground = 0; // 0表示左下到右上场地，1表示右下到左上场地
+        float _block_distance = 0.23f; // 视作障碍物的距离阈值，单位米
+        float _current_distance = 0.5f; // 当前超声波测距值，单位米
+        // 5行8列的迷宫地图(0:空地, 1:障碍物)
+        std::array<std::array<int, 8>, 5> _map;// 该地图的0,0为左上角，x正方向向下，y正方向向右
+        int _x, _y;  // 当前位置坐标
+        bool _is_step = false; // 记录是否已经走过一步
+        bool _be_block = false; // 记录被堵车，重点标志位，宝龙记得把这个成员变量丢到主函数里面，如果发现被堵住了，就接收我的转向信号
+        int _be_block_time = 0; // 记录被堵车的次数
+        // bool _is_obstacle_detected = false; // 记录是否探测过障碍物
+        Dir _prior_dir[4] = {UP, RIGHT, DOWN, LEFT}; // 默认左下到右上的方向优先级
+        Dir _forbid_dir = DOWN; // 禁止出界方向
+        Dir _current_dir; // 当前车头朝向(用于探测障碍物)
+        Maze(int whichground,float block_distance) : _whichground(whichground), _block_distance(block_distance)
+        {
+            if(_whichground==0)
+            {
+                _x = 4; // 初始位置(左下角)
+                _y = 0;
+
+            }
+            else if(_whichground==1)
+            {
+                _x = 4; // 初始位置(右下角)
+                _y = 7;
+                _prior_dir[1]= LEFT;
+                _prior_dir[3]= RIGHT;
+            }
+            // 初始化地图为全空地
+            for (auto& row :_map) row.fill(0);
+        }
+        void update_next_dir(); // 刷新要走的方向
+        bool detect_obstacle(); // 探测障碍物,返回这次在这个方向是否探测到一步的障碍物
+        void update_self_position(); // 更新当前位置
+
+};
+#endif
+
+
